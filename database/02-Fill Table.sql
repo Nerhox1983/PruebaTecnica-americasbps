0cee015131d3757f533bd41deb77ab064fb29dbd
Sergio Caceres <serjocaci@gmail.com>
2026-06-12 16:47:09 -0500

    chore: agregar script SQL de inicialización de proveedores

---
+++ database/02-Fill Table.sql
@@ -0,0 +1,4 @@
+INSERT INTO proveedores (nit_empresa, nombre_empresa, pais, tipo_carga, estado_registro)
+VALUES 
+('900123456-1', 'Distribuidora Global S.A.S', 'Colombia', 'MANUAL', 'ACTIVO'),
+('800987654-2', 'Tecnologías del Pacífico', 'Chile', 'MANUAL', 'INACTIVO');
